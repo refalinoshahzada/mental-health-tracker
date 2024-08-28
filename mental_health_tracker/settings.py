87e641e2170f4d27cb907cecacf659610abb3f69
refalinoshahzada <ref4gha@gmail.com>
2024-08-28 14:50:28 +0700

    last commit

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c1w2zyz@l6#$jk&x&s5gy*2@c^(uxjyqzil85u(ds2sau7_@y9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "<refalino-shahzada-mentalhealthtracker.pbp.cs.ui.ac.id>"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "refalino-shahzada-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 
 # Application definition
